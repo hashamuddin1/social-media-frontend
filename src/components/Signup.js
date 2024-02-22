cbaed23c625a40af2e520b5717418fe1ba3c5fcf
hashamuddin1 <hashamlaptop@gmail.com>
2024-02-23 02:55:06 +0500

    start creating signup page

---
+++ src/components/Signup.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../App.css"
+
+export default function Signup() {
+  return (
+    <>
+    <div className='signup-flex'>
+    <div className='left-corner-signup'>
+        
+    </div>
+    <div className='right-corner-signup'>
+        
+    </div>
+    </div>
+    </>
+  )
+}
