cfdfbf43b3cb8c6aa2baa2a7a8faf3ba2a45cdf2
hashamuddin1 <hashamlaptop@gmail.com>
2024-02-23 04:42:33 +0500

    change image in signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import "../App.css"
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import auth from '../assets/auth.jpg';
+import auth from '../assets/auth.png';
 
 export default function Signup() {
   return (
